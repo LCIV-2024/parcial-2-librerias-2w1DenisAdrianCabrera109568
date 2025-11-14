14f04fa0f0ef62a1f79882ead88463ec76e6daa5
Denis Adrian Cabrera 2w1 190568 <81592086+2w1DenisAdrianCabrera109568@users.noreply.github.com>
2025-11-14 19:02:58 -0300

    UserController
    punto UserControler

--- src/main/java/com/example/libreria/controller/UserController.java
+++ src/main/java/com/example/libreria/controller/UserController.java
@@ -21,16 +21,25 @@ public class UserController {
     @PostMapping
     public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO requestDTO) {
        // TODO: Implementar la creación de un usuario
+        //Resolucion:
+       UserResponseDTO createdUser = userService.createUser(requestDTO);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
     
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long id) {
         // TODO: Implementar la obtención de un usuario por su ID
+        //Resolucion:
+        UserResponseDTO user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
     }
     
     @GetMapping
     public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
         //TODO: Implementar la obtención de todos los usuarios
+        //Resolucion:
+        List<UserResponseDTO> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
     }
     
     @PutMapping("/{id}")
@@ -38,11 +47,17 @@ public class UserController {
             @PathVariable Long id,
             @Valid @RequestBody UserRequestDTO requestDTO) {
         //TODO: Implementar la actualización de un usuario
+        //Resolucion:
+        UserResponseDTO updatedUser = userService.updateUser(id, requestDTO);
+        return ResponseEntity.ok(updatedUser);
     }
     
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         //TODO: Implementar la eliminación de un usuario
+        //Resolucion:
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
 
